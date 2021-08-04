a64be9aa67eb11e73fd4f44ed6c1fb80d815f480
David Salonga <fdc.salonga@gmail.com>
2021-08-04 19:35:36 +0800

    [practice06] Francis feat: added introduce with no class  method for Teacher

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -2,9 +2,15 @@ package practice06;
 
 public class Teacher extends Person{
     private Klass klass;
+
     public Teacher(String name, int age, Klass klass) {
         super(name, age);
         this.klass = klass;
+
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
 
     }
 
@@ -14,7 +20,11 @@ public class Teacher extends Person{
     }
 
     public String introduce() {
+
+    if(klass != null){
         return String.format("My name is %s. I am %d years old. I am a Teacher. I teach %s.", getName(), getAge(), getKlass().getDisplayName());
+    } else
+    return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.", getName(), getAge());
 
         //"My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2."
     }
