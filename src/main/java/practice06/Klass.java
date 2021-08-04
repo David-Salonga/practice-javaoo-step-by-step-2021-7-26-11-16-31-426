c226810d0ddf092b73369d4a430eabe67baf750d
David Salonga <fdc.salonga@gmail.com>
2021-08-04 19:10:14 +0800

    [practice06] Francis feat: added getDisplayName method for Klass

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -2,14 +2,27 @@ package practice06;
 
 public class Klass {
 
+    private String name;
     private int klass;
 
-    public Klass(int i) {
-        this.klass = i;
+
+
+    public Klass( int klass) {
+        this.klass = klass;
     }
 
+    public Klass(String name, int klass) {
+        this.name = name;
+        this.klass = klass;
+    }
 
     public int getNumber() {
         return klass;
+    }
+
+
+
+    public String getDisplayName() {
+        return "Class " + klass;
     }
 }
