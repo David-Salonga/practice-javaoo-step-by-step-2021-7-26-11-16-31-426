fa2668c5cdf7288e68fa0889cfb5131ae4ee29b0
David Salonga <fdc.salonga@gmail.com>
2021-08-04 22:02:33 +0800

    [practice07] Francis feat: added getLeader and assign Leader method for Klass

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,8 +1,11 @@
 package practice07;
 
 public class Klass {
+//    private static String leader;
     private String name;
     private Integer klass;
+    private String leader;
+
 
 
 
@@ -16,6 +19,8 @@ public class Klass {
     }
 
 
+
+
     public Integer getNumber() {
        return klass;
     }
@@ -23,4 +28,34 @@ public class Klass {
     public String getDisplayName() {
         return "Class " + klass;
     }
+
+//    public static void main(String[] args) {
+//        assignLeader(new Student(1, "James", 8));
+//    }
+
+    public Student assignLeader(Student student) {
+        leader = student.getName();
+//        System.out.println(leader);
+        return student;
+    }
+
+    public boolean getLeader() {
+        Student student = assignLeader(new Student(1, "Jerry", 8));
+
+        if (leader.equals(student.getName())){
+            return true;
+        } return false;
+    }
+
+
+
+
+
+
+//    The klass attribute is an Object which has number and leader attribute. But the leader attribute is not in the constructor.
+//    When you build an instance of Student you need pass the Klass instance to the constructor of Student.
+//    You can find more details in the test case. The Klass has an assignLeader method, which will take an instance of Student as parameter.
+//    It means setting that student as the leader of the Klass. If the leader of Klass is Tom, then Tom calls the introduce method should return:
+
+
 }
