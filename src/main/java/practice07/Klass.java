e3698c935f88fb6583475d19e73089d245478dca
David Salonga <fdc.salonga@gmail.com>
2021-08-04 21:06:32 +0800

    [practice07] Francis feat: added getNumber method for Klass

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,22 @@
 package practice07;
 
 public class Klass {
+    private String name;
+    private Integer klass;
+
+
+
+    public Klass( int klass) {
+        this.klass = klass;
+    }
+
+    public Klass(String name, int klass) {
+        this.name = name;
+        this.klass = klass;
+    }
+
+
+    public Integer getNumber() {
+       return klass;
+    }
 }
