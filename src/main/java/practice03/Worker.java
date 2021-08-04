0f121830bdf2c0fb2b58d9cbaa4b8c44bf65fa4b
David Salonga <fdc.salonga@gmail.com>
2021-08-04 16:38:36 +0800

    [practice03] Francis feat: added getName and getAge method for Worker

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,27 @@
 package practice03;
 
 public class Worker {
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
