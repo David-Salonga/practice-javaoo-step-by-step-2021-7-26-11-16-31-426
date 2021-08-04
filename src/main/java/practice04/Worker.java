bade7386b7728df2b7177e6e62740db5e05b0f06
David Salonga <fdc.salonga@gmail.com>
2021-08-04 18:07:07 +0800

    [practice04] Francis feat: added introduce,getName,getAge  method for Worker

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,27 @@
 package practice04;
 
 public class Worker {
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a Worker. I have a job.", name, age);
+
+        //"My name is Tom. I am 21 years old. I am a Worker. I have a job."
+//        String.format("My name is %s. I am %d years old.", name, age);
+    }
 }
