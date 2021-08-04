f7119449e91e7cfa9059de5a4f7367b20dc7264a
David Salonga <fdc.salonga@gmail.com>
2021-08-04 21:03:16 +0800

    [practice07] Francis feat: added equals method for Person

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,5 +1,7 @@
 package practice07;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private int age;
@@ -21,5 +23,18 @@ public class Person {
 
     public String introduce() {
         return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, id);
     }
 }
